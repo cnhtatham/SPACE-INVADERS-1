b3243a564e6e35c57b1f670122823c403bd6a1f3
Ben Maudslay <benmaudslay@gmail.com>
2018-02-19 11:11:33 +0000

    Revert "Updates - sound and removal"
    This reverts commit 2768a8314aff5942cf530c7c83dae7c8d9d73e2d.

--- script.js
+++ script.js
@@ -4,18 +4,14 @@ var ctx = canvas.getContext("2d");
 var ship = document.getElementById("ship");
 ship.style.display = 'none';
 
-var explosion = document.getElementById("explode");
-explosion.style.display = "none";
-
 var shot = document.getElementById("fireSound");
-var iKilled = document.getElementById("killed");
 
-var x = canvas.width / 2;
-var y = canvas.height - 55
+var x = canvas.width/2;
+var y = canvas.height-55
 var x2 = x2
-var y2 = canvas.height - 30
+var y2 = canvas.height-30
 var dx = 2;
-var dy = -2;
+var dy =-2;
 var ballRadius = 10
 var rightPressed = false
 var leftPressed = false
@@ -34,16 +30,12 @@ var moveLeft = true;
 var moveRight = false;
 
 var invaders = []; // create a 2d array of space invaders
-for (c = 0; c < invaderColumnCount; c++) {
-    invaders[c] = [];
-    for (r = 0; r < invaderRowCount; r++) {
-        invaders[c][r] = {
-            x: 0,
-            y: 0,
-            status: 1
-        }
-    }
+for (c=0; c<invaderColumnCount; c++) {
+invaders[c] = [];
+for (r=0; r<invaderRowCount; r++) {
+    invaders[c][r] = { x:0, y:0, status: 1}
 }
+} 
 
 
 document.addEventListener("keydown", keyDownHandler, false);
@@ -51,52 +43,62 @@ document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("keydown", spaceBarHandler, false)
 
 function keyDownHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = true;
-    } else if (e.keyCode == 37) {
-        leftPressed = true;
-    }
+if(e.keyCode == 39) {
+    rightPressed = true;
+}
+else if(e.keyCode == 37) {
+    leftPressed = true;
+}
 }
 
 function keyUpHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = false;
-    } else if (e.keyCode == 37) {
-        leftPressed = false;
-    }
+if(e.keyCode == 39) {
+    rightPressed = false;
+}
+else if(e.keyCode == 37) {
+    leftPressed = false;
+}
 }
 
 function spaceBarHandler(e) {
-    if (e.keyCode == 32) {
-        spacePressed = true;
-        bulletActive = false;
-    }
+if(e.keyCode == 32) {
+    spacePressed = true;
+    bulletActive = false;
+}
 }
 
 
 function drawShip() {
+<<<<<<< HEAD
 
     ctx.beginPath();
     ctx.drawImage(ship, x, y, 50, 50);
     ctx.closePath();
+=======
+ctx.beginPath();
+ctx.drawImage(ship, x, y, 50, 50);
+ctx.closePath();
+>>>>>>> parent of 2768a83... Updates - sound and removal
 }
 
+
+
 function drawBullet() {
-    if (bulletActive == false) {
-        ctx.beginPath();
-        ctx.rect(x2, y2 - 25, 1, 15);
-        ctx.fillStyle = "black"
-        ctx.fill();
-        ctx.stroke();
-        ctx.closePath;
-        bulletCount++
-        if (y2 < 0) {
-            spacePressed = false;
-            y2 = canvas.height - 30;
-            bulletCount = 0
-        }
+if (bulletActive == false) {
+    ctx.beginPath();
+    ctx.rect(x2, y2 - 25, 1, 15);
+    ctx.fillStyle = "black"
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath;
+    bulletCount ++
+    if (y2 < 0) {
+        spacePressed = false;
+        y2 = canvas.height-30;
+        bulletCount = 0
     }
 }
+}   
 
 
 function moveInvaders () {
@@ -127,35 +129,26 @@ function sideDetection() {
 }
 
 function drawInvaders() { //create a 2 day array and paint each invader in it's location
-    for (c = 0; c < invaderColumnCount; c++) {
-        for (r = 0; r < invaderRowCount; r++) {
-            if (invaders[c][r].status == 1) {
-                var invaderX = (c * (invaderWidth + invaderPadding)) + invaderOffsetLeft;
-                var invaderY = (r * (invaderHeight + invaderPadding)) + invaderOffsetTop;
-                invaders[c][r].x = invaderX
-                invaders[c][r].y = invaderY
-                ctx.beginPath();
-                ctx.rect(invaderX, invaderY, invaderWidth, invaderHeight);
-                ctx.fillStyle = 'green';
-                ctx.fill();
-                ctx.closePath();
+    for(c=0; c<invaderColumnCount; c++) {
+        for(r=0; r<invaderRowCount; r++) {
+            if(invaders[c][r].status == 1) {
+            var invaderX = (c*(invaderWidth+invaderPadding))+invaderOffsetLeft;
+            var invaderY = (r*(invaderHeight+invaderPadding))+invaderOffsetTop;
+            invaders[c][r].x = invaderX
+            invaders[c][r].y = invaderY
+            ctx.beginPath();
+            ctx.rect(invaderX, invaderY, invaderWidth, invaderHeight);
+            ctx.fillStyle = 'green';
+            ctx.fill();
+            ctx.closePath();
             }
         }
     }
-}
-
-function drawExplosion() {
-    ctx.beginPath();
-    ctx.drawImage(explosion, i.x, i.y);
-    ctx.closePath();
-}
-
-function removeExplosion() {
-    explosion.style.display = "none";
-}
+    }
 
 // check each invader if the bullet has hit
 function collisionDetection() {
+<<<<<<< HEAD
     for (c = 0; c < invaderColumnCount; c++) {
         for (r = 0; r < invaderRowCount; r++) {
             var i = invaders[c][r];
@@ -176,18 +169,38 @@ function collisionDetection() {
                     drawExplosion();
                     setTimeout(removeExplosion, 3000)
                 }
+=======
+        for(c=0; c<invaderColumnCount; c++) {
+            for(r=0; r<invaderRowCount; r++) {
+                var i = invaders[c][r];
+                if (i.status == 1) {
+//check to see if the bullets x value is greater than the x position of the invader including it's width, then check if the bullet has reached it's y value plus 27 for better effect.
+                    if(x2 > i.x && x2 < i.x + invaderWidth && y2 > i.y && y2 < i.y + invaderHeight + 27) {
+                        i.status = 0
+                        bulletActive = true;
+                        spacePressed = false;
+                        y2 = canvas.height-30;
+                        bulletCount = 0
+                        score ++;
+                        if(score == 55) {
+                            alert("YOU WIN, CONGRATULATIONS!");
+                            document.location.reload();
+                        }
+                    }
+                }  
+>>>>>>> parent of 2768a83... Updates - sound and removal
             }
         }
-    }
 }
 
 function drawScore() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: " + score, 8, 20);
+    ctx.fillText("Score: "+score, 8, 20);
 }
 
 function draw() {
+<<<<<<< HEAD
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawInvaders();
@@ -215,6 +228,32 @@ function draw() {
         y2 -= 6; //bullet will travel up the screen
         drawBullet();
     }
+=======
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+drawInvaders();
+drawShip();
+drawScore();
+collisionDetection();
+
+// stops ball moving too far
+if(rightPressed && x < canvas.width-ballRadius || rightPressed && x < ballRadius) {
+x += 3;
+} 
+else if(leftPressed && x > 0 + ballRadius || leftPressed && x > ballRadius) {
+x -= 3;
+}
+
+//During firing
+if(spacePressed) {
+if (bulletCount === 0) { //Take the first x position of the ship at fire
+    x2 = x + 24.5;
+    //audio
+    shot.play();
+}
+y2 -=6; //bullet will travel up the screen
+drawBullet();
+} 
+>>>>>>> parent of 2768a83... Updates - sound and removal
 
 } //end of draw
 
